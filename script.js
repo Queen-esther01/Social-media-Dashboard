ec1d6c8d9c85d9cddab0956cc37580ad83cd061c
Ejidike Esther <ejidikeesther@gmail.com>
2020-05-03 22:53:47 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// const input = document.querySelector('#input').checked;
+// const slider = document.querySelector('.slider');
+// if(input === true){
+//     console.log('input is checked')
+//     slider.style.background = 'red';
+// }
