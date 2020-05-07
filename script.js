be30875a879c322f60c1a853b80486c59b9e87cb
Ejidike Esther <esteygirl56@gmail.com>
2020-05-07 02:05:48 +0100

    Add alert message

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// const input = document.querySelector('#input').checked;
-// const slider = document.querySelector('.slider');
-// if(input === true){
-//     console.log('input is checked')
-//     slider.style.background = 'red';
-// }
+Alert(`Toggle mode does not work yet, so does the cool popup I was supposed to display this message on, I'll take care of that soon, thank you for understanding :)`)
