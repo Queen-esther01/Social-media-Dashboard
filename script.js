7970b26c6799b7e3a5d4dbea61797c8c0542b998
Ejidike Esther <esteygirl56@gmail.com>
2020-05-07 02:13:41 +0100

    Update alert with html symbol

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-Alert(`Toggle mode does not work yet, so does the cool popup I was supposed to display this message on, I'll take care of that soon, thank you for understanding :)`)
+Alert(`Toggle mode does not work yet, so does the cool popup I was supposed to display this message on, I'll take care of that soon, thank you for understanding &#9786;`)
