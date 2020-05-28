e50b227bb7c8f6f7f2f2e345a631e81c3625468d
Ejidike Esther <ejidikeesther@gmail.com>
2020-05-28 23:15:42 +0100

    change toggle text

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let html = document.documentElement;
-let mode = document.getElementsByClassName('dark-mode').textContent;
+let mode = document.getElementById('dark-mode');
 let checkbox = document.querySelector('input[type=checkbox]');
 checkbox.addEventListener('change', () => {
     if(checkbox.checked){
-        mode = 'Light Mode';
         transition()
+        mode.textContent = 'Light Mode';
         html.setAttribute('data-theme', 'light');
     } else{
         transition()
+        mode.textContent = 'Dark Mode';
         html.setAttribute('data-theme', 'dark');
     }
 })
